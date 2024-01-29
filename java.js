f5894366327c357206e68df4e34dbbe7dadd1d29
alisajid0023 <155763126+alisajid0023@users.noreply.github.com>
2024-01-29 17:56:30 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,25 @@
+// ASS_3
+
+let temp = prompt("Current temperature in celsius")
+if (temp>=30) {
+    alert("It's a hot day");
+    
+}
+    else if(temp<30) {
+
+    alert("Weather is moderate")
+} 
+
+
+
+let marks = prompt ("Your marks")
+if(marks<=100){
+    console.log("Sorry better luck next time")
+ } else if (marks>100 && marks<=200){
+     console.log("Your grade is C")
+ } else if (marks>200 && marks<=300){
+     console.log("Your grade is B")
+ } else if (marks>300 && marks<=400){
+     console.log("Your grade is A")
+ }
+     // ASS_3
